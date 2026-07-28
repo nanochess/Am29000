7f346dffdc895cacb4ad5097379a2334c2fa2c81
Oscar Toledo <biyubi@gmail.com>
2026-07-27 18:04:58 -0600

    Initial Commit

---
+++ am29000/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  am29000
+//
+//  Created by Oscar Toledo on 27/07/26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
